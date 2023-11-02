2f256ac87ecf499ac5349a0937cbff26ce931076
Younes Outerbah <younesouterbah1@gmail.com>
2023-11-02 19:02:05 +0100

    added dark mode to the control

--- src/components/control/Control.jsx
+++ src/components/control/Control.jsx
@@ -3,7 +3,7 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { BiMenu } from "react-icons/bi";
 import { BsMoonStars, BsBell, BsSun } from "react-icons/bs";
 import profileImage from "/image/profile.png";
-import { useContext} from "react";
+import { useContext } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
 
 export const Control = ({ setMenuToggle }) => {
@@ -12,17 +12,24 @@ export const Control = ({ setMenuToggle }) => {
   return (
     <div className="control">
       <div className="menu-search">
-        <BiMenu className="menu" onClick={() => setMenuToggle(true)} />
-        <div className="search">
-          <AiOutlineSearch className="icon" />
+        <BiMenu
+          className={theme === "light" ? "menu" : "menu dark"}
+          onClick={() => setMenuToggle(true)}
+        />
+        <div className={theme === "light" ? "search" : "search dark"}>
+          <AiOutlineSearch className={theme === "light" ? "icon" : "icon dark"} />
           <input type="search" name="" id="" />
         </div>
       </div>
       <div className="profile">
         <div className="mode" onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-          {theme === "light" ? <BsMoonStars className="icon" /> : <BsSun className="icon" />}
+          {theme === "light" ? (
+            <BsMoonStars className="icon" />
+          ) : (
+            <BsSun className={theme === "light" ? "icon" : "icon dark"} />
+          )}
         </div>
-        <BsBell className="icon" />
+        <BsBell className={theme === "light" ? "icon" : "icon dark"} />
         <div className="img">
           <img src={profileImage} alt="" />
         </div>
